67f3e2a7e4d58db3dfe2df1e07754f24b9c13255
Simon J <simon.jungherz@stud.tu-darmstadt.de>
2022-11-13 18:29:52 +0000

    Add lint/format configs

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    env: {
+        node: true,
+        es2020: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+    },
+    plugins: ['@typescript-eslint'],
+    root: true,
+};
---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+// https://prettier.io/docs/en/options.html
+// these are mostly just the defaults but specified anyway because why not
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: 'always',
+    endOfLine: 'lf',
+};
