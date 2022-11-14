386db9361c96d167c109706c4bdfe977c2e29d37
Dead_Cowboy <laurenz.kammeyer@gmx.de>
2022-11-14 17:51:49 +0100

    Fixed dangeling promises in index

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { setupDatabase } from './db/db_setup';
 import { User } from './db/db_model';
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 
+// Attempt at Decorator. But TS doesnt like me
 // function catchPromise(target:Promise<any>,catchFunction:Function = (error:any) => {console.log("Promise failded!")}) {
 //     return (target:Promise<any>) => {target.catch((error:any) => catchFunction(error))}
 // }
@@ -14,15 +15,13 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
-    // TODO fix this at some point:
-
-    User.createNewUser('Jonas', 'Test').catch(() => console.log('Error on User creation'));
-
-    res.send('<h1> Initial setup </h1>');
+    User.createNewUser('Jonas', 'Test')
+        .then(() => res.send('<h1> Initial setup </h1>'))
+        .catch(() => console.log('Error on User creation'));
 });
 
 app.listen(port, () => {
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    setupDatabase().catch(() => console.log('Error on Database setup'));
-    console.log(`[server]: Server is running at https://localhost:${port!}`);
+    setupDatabase()
+        .then(() => console.log(`[server]: Server is running at https://localhost:${port!}`))
+        .catch(() => console.log('Error on Database setup'));
 });
