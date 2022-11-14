c98a3a387ab7bd0de4d8fe59e30afb393acf589e
Simon J <simon.jungherz@stud.tu-darmstadt.de>
2022-11-14 18:31:43 +0000

    Improve async router handling

--- src/db/db_setup.ts
+++ src/db/db_setup.ts
@@ -1,21 +1,13 @@
 import { sequelize } from './db_model';
 
 async function testConnection() {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
 }
 
 export async function setupDatabase() {
-    try {
-        await testConnection();
-        // Synchronize whole model to DB
-        await sequelize.sync({ force: true });
-        console.log('All models were synchronized successfully.');
-    } catch (error) {
-        console.error('Unable to match the data to the model:', error);
-    }
+    await testConnection();
+    // Synchronize whole model to DB
+    await sequelize.sync({ force: true });
+    console.log('All models were synchronized successfully.');
 }
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,35 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import express, { Express, Request, Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
 import { setupDatabase } from './db/db_setup';
 import { User } from './db/db_model';
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
 
-// Attempt at Decorator. But TS doesnt like me
-// function catchPromise(target:Promise<any>,catchFunction:Function = (error:any) => {console.log("Promise failded!")}) {
-//     return (target:Promise<any>) => {target.catch((error:any) => catchFunction(error))}
-// }
+/** Properly handles async errors in express routers */
+function asyncHandler(fn: (req: Request, res: Response) => Promise<void>) {
+    return function (req: Request, res: Response, next: NextFunction): void {
+        fn(req, res).catch(next);
+    };
+}
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-    User.createNewUser('Jonas', 'Test')
-        .then(() => res.send('<h1> Initial setup </h1>'))
-        .catch(() => console.log('Error on User creation'));
-});
+app.get(
+    '/',
+    asyncHandler(async (req: Request, res: Response) => {
+        await User.createNewUser('Jonas', 'Test');
 
-app.listen(port, () => {
-    setupDatabase()
-        .then(() => console.log(`[server]: Server is running at https://localhost:${port!}`))
-        .catch(() => console.log('Error on Database setup'));
-});
+        res.send('<h1> Initial setup </h1>');
+    })
+);
+
+async function init() {
+    await setupDatabase();
+
+    app.listen(port, () => {
+        console.log(`Server is running at https://localhost:${port!}`);
+    });
+}
+
+void init();
