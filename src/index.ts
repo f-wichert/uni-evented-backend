b643a960b5934b1fe86af49b31a0605ceae1a3dd
js <skylinertwo@aol.com>
2022-11-15 14:55:45 +0100

    implement very basic media upload and conversion to m3u8 at /upload

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,11 @@ dotenv.config();
 import express, { Express, NextFunction, Request, Response } from 'express';
 import { setupDatabase } from './db';
 import { User } from './db/models/user';
+import { Clip } from './db/models/clip';
 import ffmpeg from 'fluent-ffmpeg';
+import fileUpload from 'express-fileupload';
+import fs from 'fs';
+import { create } from 'domain';
 
 /** Properly handles async errors in express routers */
 function asyncHandler(fn: (req: Request, res: Response) => Promise<void>) {
@@ -13,8 +17,19 @@ function asyncHandler(fn: (req: Request, res: Response) => Promise<void>) {
     };
 }
 
-const app: Express = express();
+const MEDIA_ROOT = process.env.MEDIA_ROOT || 'media';
+const MEDIA_UPLOAD_ROOT = process.env.MEDIA_UPLOAD_ROOT || 'uploads';
+const CLIP_UPLOAD_INPUT_NAME_FIELD = (process.env.CLIP_UPLOAD_INPUT_NAME_FIELD = 'clip');
+const FFMPEG_TIMEOUT = Number.parseInt(process.env.FFMPEG_TIMEOUT || '60');
+
 const port = process.env.PORT;
+
+const app: Express = express();
+app.use(
+    fileUpload({
+        limits: { fileSize: 50 * 1024 * 1024 },
+    })
+);
 
 app.get(
     '/',
@@ -25,27 +40,81 @@ app.get(
     })
 );
 
-app.get('/upload', (req, res) => {
-    console.log('lol');
-    ffmpeg('uploads/input.mp4', { timeout: 20 })
-        .addOptions([
-            '-level 3.0',
-            '-s 640x360',          // 640px width, 360px height output video dimensions
-            '-hls_time 1',        // 10 second segment duration
-            '-hls_list_size 0',    // Maxmimum number of playlist entries (0 means all entries/infinite)
-            '-f hls', // HLS format
-        ])
-        .output('uploads/output.m3u8')
-        .on('end', () => console.log('test'))
-        .run();
-    res.send('');
-});
+app.post(
+    '/upload',
+    asyncHandler(async (req, res) => {
+        if (!req.files || !(Object.keys(req.files).length === 1)) {
+            console.log('no files uploaded');
+            res.status(400).send('no files uploaded');
+            return;
+        }
+
+        const file = req.files[CLIP_UPLOAD_INPUT_NAME_FIELD];
+
+        if (Array.isArray(file)) {
+            console.log('file is array');
+            res.status(400).send('file is an array');
+            return;
+        }
+
+        const clip = await Clip.create();
+        const upload_path = `${MEDIA_UPLOAD_ROOT}/${clip.id}`;
+        const clip_path = `${MEDIA_ROOT}/clips/${clip.id}`;
+
+        try {
+            await createDirectory(clip_path);
+        } catch (error) {
+            console.log(`something went wrong trying to create dir ${clip_path}: ${error}`);
+            await clip.destroy();
+            return;
+        }
+
+        await file.mv(upload_path);
+
+        ffmpeg(upload_path, { timeout: FFMPEG_TIMEOUT })
+            .addOption([
+                '-level 3.0',
+                `-hls_time ${process.env.CLIP_HLS_SEGMENT_DURATION}`,
+                '-hls_list_size 0',
+                '-f hls',
+            ])
+            .size(process.env.CLIP_MAX_RESOLUTION!)
+            .output(`${clip_path}/index.m3u8`)
+            .on('error', (error) => console.log('ffmpeg error: ', error))
+            .on('end', () => {
+                console.log('done processing ', upload_path, ' => ', clip_path);
+                clip.file_available = true;
+                clip.save().then(() => console.log(`clip ${clip.id} now available`));
+            })
+            .run();
+
+        res.send('');
+    })
+);
+
+async function createDirectory(path: string) {
+    try {
+        await fs.promises.access(path);
+    } catch (error) {
+        // do nothing, directory probably already exists
+    }
+
+    try {
+        await fs.promises.mkdir(path, { recursive: true });
+    } catch (error) {
+        throw error;
+    }
+}
 
 async function init() {
     await setupDatabase();
 
+    // create media directories if they don't exists
+    await createDirectory(MEDIA_ROOT + '/clips');
+    await createDirectory(MEDIA_UPLOAD_ROOT);
+
     app.listen(port, () => {
-        console.log(`Server is running at https://localhost:${port!}`);
+        console.log(`Server is running at http://localhost:${port!}`);
     });
 }
 
