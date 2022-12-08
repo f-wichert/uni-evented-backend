4225a7ddbf810dc5524ca934c5d0560287860a2e
sj <simon.jungherz@stud.tu-darmstadt.de>
2022-12-08 19:44:01 +0000

    fix: remove email test endpoint

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,10 @@ import fileUpload from 'express-fileupload';
 import fs from 'fs';
 import morgan from 'morgan';
 import passport from 'passport';
-import { z } from 'zod';
 
 import config from './config';
 import { connect } from './db';
-import User from './db/models/user';
 import routes from './routes';
-import { sendMail } from './utils/email';
-import { validateBody } from './utils/validate';
 
 const app = express();
 
@@ -33,14 +29,8 @@ app.use(
 // mount routers
 app.use('/api', routes);
 
-app.get('/', async (req, res) => {
-    await User.findAll();
-    res.json({ things: 'stuff' });
-});
-
-app.post('/testemail', validateBody(z.object({ email: z.string() })), async (req, res) => {
-    await sendMail(req.body.email, 'Test Mail', { text: 'hi there' });
-    res.sendStatus(200);
+app.get('/', (req, res) => {
+    res.json({ status: 'ok' });
 });
 
 async function init() {
