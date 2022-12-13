f6765854f33d19dd130665621a45455df9c4e450
Dead_Cowboy <laurenz.kammeyer@gmx.de>
2022-12-13 15:41:23 +0100

    resolved merge conflicts again

--- src/routes/info.ts
+++ src/routes/info.ts
@@ -4,20 +4,9 @@ import Media from '../db/models/media';
 
 const router = Router();
 
-<<<<<<< HEAD
-router.get(
-    '/all_media',
-    asyncHandler(async (req, res) => {
-        const media = await Media.findAll();
-        console.log(`Media: ${media}`)
-        res.json({ media: media });
-    })
-);
-=======
 router.get('/all_media', async (req, res) => {
     const media = await Media.findAll();
     res.json({ media: media });
 });
->>>>>>> cdb91aa5fa115075d90812e8f9a9d6aa5d678c4a
 
 export default router;
