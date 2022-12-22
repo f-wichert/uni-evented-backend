114a2213d6b2f5682331d80e45308ca8668c7ef2
sj <simon.jungherz@stud.tu-darmstadt.de>
2022-12-22 15:26:43 +0000

    feat: improve error handler
    - log errors to console again
    - log warning if headers were already sent
    - use message from non-HttpErrors before falling back to default message in dev mode
    - take HttpError.expose into account

--- src/index.ts
+++ src/index.ts
@@ -45,24 +45,40 @@ app.use((req, res, next) => {
 });
 
 // needs explicit `ErrorRequestHandler` due to https://github.com/DefinitelyTyped/DefinitelyTyped/issues/4212
-const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+const errorHandler: ErrorRequestHandler = (err: unknown, req, res, next) => {
+    // get stack from error
+    let stack: string[] | null = null;
+    const stackStr = (err as Error)?.stack;
+    if (stackStr && typeof stackStr === 'string') {
+        stack = stackStr.split('\n');
+    }
+
+    // `setTimeout(..., 0)` to log the error *after* the access log line,
+    // instead of before, which would be confusing
+    setTimeout(() => console.log(stackStr || err), 0);
+
     // just forward and do nothing if headers are already set, since it'd be too late to set a status now
-    if (res.headersSent) return next(err);
+    if (res.headersSent) {
+        console.warn('Cannot set response code from error, headers were already sent.');
+        return next(err);
+    }
+
+    const isDev = config.NODE_ENV !== 'production';
 
     // take code + message from error if possible
     let code = 500;
-    let message = 'Internal Server Error';
+    let message: string | null = null;
     if (httpError.isHttpError(err)) {
         code = err.statusCode;
-        message = err.message;
+        if (err.expose) message = err.message;
+    } else if (isDev) {
+        message = (err as Error)?.message;
     }
 
-    const body: { error: string; stack?: string[] } = { error: message };
-
+    const body: { error: string; stack?: string[] } = { error: message || 'Internal Server Error' };
     // add stack to response, if not running in production mode
-    const stack = (err as Error).stack;
-    if (config.NODE_ENV !== 'production' && stack && typeof stack === 'string') {
-        body.stack = stack.split('\n');
+    if (isDev && stack) {
+        body.stack = stack;
     }
 
     res.status(code).json(body);
