34e7bba95ebe6cc8d84059ab0c570bc4d311b5e5
sj <simon.jungherz@stud.tu-darmstadt.de>
2023-01-24 13:35:27 +0000

    fix: remove `value` attribute from tag response

--- src/routes/event.ts
+++ src/routes/event.ts
@@ -36,7 +36,7 @@ async function getEventForResponse(id: string) {
             {
                 model: Tag,
                 as: 'tags',
-                attributes: ['id', 'label', 'color', 'value', 'parent'],
+                attributes: ['id', 'label', 'color', 'parent'],
             },
         ],
     });
