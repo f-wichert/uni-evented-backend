498d0ce94f14a76410a39757c5f092a8b52912af
sj <simon.jungherz@stud.tu-darmstadt.de>
2023-01-30 23:41:55 +0000

    test: sketch out remaining auth tests

--- tests/routes/auth.spec.ts
+++ tests/routes/auth.spec.ts
@@ -11,7 +11,7 @@ beforeEach(async () => {
     await setupDatabase(true);
 });
 
-describe('POST /register', () => {
+describe('POST /auth/register', () => {
     const register = () => request(app).post('/api/auth/register');
 
     it('successfully creates users', async () => {
@@ -29,11 +29,47 @@ describe('POST /register', () => {
         expect(res.body).toEqual({ error: 'Username already taken' });
     });
 
-    it('should not allow duplicate emails', async () => {
+    it('rejects duplicate emails', async () => {
         await createUser('duplicate2');
         const res = await register()
             .send({ username: 'otherduplicate', email: 'duplicate2@abc.com', password: 'test1234' })
             .expect(409);
         expect(res.body).toEqual({ error: 'User with given email already exists' });
     });
+});
+
+describe('POST /auth/login', () => {
+    const login = () => request(app).post('/api/auth/login');
+
+    it.todo('allows login by username (case-insensitive)');
+
+    it.todo('allows login by email');
+
+    it.todo('supports password reset tokens');
+
+    it.todo('rejects unknown users');
+
+    it.todo('rejects invalid passwords');
+});
+
+describe('POST /auth/reset', () => {
+    const reset = () => request(app).post('/api/auth/reset');
+
+    it.todo('rejects unknown users');
+
+    it.todo('creates a token and sends an email');
+});
+
+describe('POST /auth/registerPush', () => {
+    const registerPush = () => request(app).post('/api/auth/registerPush');
+
+    it.todo('inserts new tokens');
+
+    it.todo('updates the userId of existing tokens');
+});
+
+describe('POST /auth/unregisterPush', () => {
+    const unregisterPush = () => request(app).post('/api/auth/unregisterPush');
+
+    it.todo('removes tokens');
 });
